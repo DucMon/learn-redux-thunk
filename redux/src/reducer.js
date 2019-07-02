50fa0ca5a6cb516c9127c6bcfed56fd724a50fdf
Duc Nguyen <immonnguyen@gmail.com>
2019-07-02 15:37:15 +0700

    add reducers

--- redux/src/reducer.js
+++ redux/src/reducer.js
@@ -1,17 +1,42 @@
-import { VisibilityFilters } from "./actions";
+import {
+  VisibilityFilters,
+  SET_INVISIBILITY_FILTER,
+  ADD_TODO,
+  TOGGLE_TODO
+} from "./actions";
 
 const initialState = {
   visibilityFilters: VisibilityFilters.SHOW_ALL,
   todos: []
 };
 
-function todoApp(state, action) {
-  if (typeof state === "undefined") {
-    return initialState;
-  }
-  return state;
-}
+// function todoApp(state, action) {
+//   if (typeof state === "undefined") {
+//     return initialState;
+//   }
+//   return state;
+// }
 
 function todoApp(state = initialState, action) {
-  return state;
+  switch (action.type) {
+    case SET_INVISIBILITY_FILTER:
+      return Object.assign({}, state, { visibilityFilters: action.filter });
+    case ADD_TODO:
+      return Object.assign({}, state, {
+        todos: [...state.todos, { text: action.text, completed: false }]
+      });
+    case TOGGLE_TODO:
+      return Object.assign({}, state, {
+        todos: state.todos.map((todo, index) => {
+          if (index === action.index) {
+            return Object.assign({}, todo, {
+              completed: !todo.completed
+            });
+          }
+          return todo;
+        })
+      });
+    default:
+      return state;
+  }
 }
